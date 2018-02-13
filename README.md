6a2723f88b27957ce7a17b97eb486b10621835e2
WISEPLAT <stroy-detal.com@yandex.ru>
2018-02-13 14:08:49 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# wsh-lib
-Lightweight Wiseplat libraries
+## Wsh-Lib
+
+Lightweight Wiseplat libraries. This is a temporary repository which will be used as the basis of an implementation on Idris (or similar).
---
+++ test/lib/benchmark.js
@@ -0,0 +1,18 @@
+const rnd = require("./randomData.js");
+const rlp = require("./../../src/rlp.js");
+const ref = {rlp: require("rlp")};
+const cps = f => {
+  for (var t = Date.now(), i = 0; Date.now() - t < 1000; ++i)
+    f();
+  return i;
+};
+
+let dataTrees = [];
+for (let i = 0; i < 64; ++i)
+  dataTrees.push(rnd.dataTree());
+console.log("Benchmarking Wsh-Lib's RLP implementation vs the one on NPM.");
+const liteCps = cps(() => dataTrees.map((dt) => rlp.decode(rlp.encode(dt))));
+const npmCps = cps(() => dataTrees.map((dt) => ref.rlp.decode(ref.rlp.encode(dt))));
+console.log("- Wsh-Lib: " + liteCps + " calls per second.");
+console.log("- Reference: " + npmCps + " calls per second.");
+console.log("- Wsh-Lib is: " + (liteCps / npmCps).toFixed(2) + " " + (liteCps > npmCps ? "faster" : "slower") + " than reference.");
